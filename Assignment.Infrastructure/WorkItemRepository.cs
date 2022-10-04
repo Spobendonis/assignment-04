867b375ab5b4c934a307f39619758563a6ac94e9
Megabd <bath@itu.dk>
2022-10-04 14:47:39 +0200

    WorkItemRepo done?, untested

--- Assignment.Infrastructure/WorkItemRepository.cs
+++ Assignment.Infrastructure/WorkItemRepository.cs
@@ -1,49 +1,166 @@
 namespace Assignment.Infrastructure;
+using Assignment.Core;
+
 
 public class WorkItemRepository : IWorkItemRepository
 {
+
+    private readonly KanbanContext _context;
+
+    public WorkItemRepository(KanbanContext context){
+        _context = context;
+    }
+
     public (Response Response, int ItemId) Create(WorkItemCreateDTO item)
     {
-        throw new NotImplementedException();
+       var entity = new WorkItem(item.Title)
+        {   
+            
+            Title = item.Title,
+            AssignedToId = item.AssignedToId,
+            Description = item.Description,
+            Tags = CreateOrUpdateTags(item.Tags).ToHashSet()
+            
+        };
+
+        var created = new WorkItemDetailsDTO(entity.Id, entity.Title, entity.Description, DateTime.UtcNow,
+         entity.AssignedTo?.Name, entity.Tags.Select(t =>t.Name).ToHashSet(), State.New, DateTime.UtcNow);
+
+        _context.Items.Add(entity);
+        _context.SaveChanges();
+
+        return (Created, created.Id);
     }
 
     public Response Delete(int itemId)
     {
-        throw new NotImplementedException();
+        var WorkItem = _context.Items.Include(t => t.Id).FirstOrDefault(t => t.Id == itemId);
+        Response response;
+
+        if (itemId is 0)
+        {
+            response = NotFound;
+        } 
+        else if(WorkItem?.State == State.New)
+        {
+            _context.Items.Remove(WorkItem);
+            _context.SaveChanges();
+            response = Deleted;
+        }
+        else if(WorkItem?.State == State.Active)
+        {
+            WorkItem.State = State.Removed;
+        }
+        else if(WorkItem?.State == State.Resolved || WorkItem?.State == State.Closed || WorkItem?.State == State.Removed)
+        {
+            response = Conflict;
+        }
+
+        response = NotFound;
+        return response;
     }
 
-    public WorkItemDetailsDTO Find(int itemId)
+    public WorkItemDetailsDTO? Find(int itemId)
     {
-        throw new NotImplementedException();
+
+        
+        var items = from i in _context.Items
+                            where i.Id == itemId
+                            select new WorkItemDetailsDTO(i.Id, i.Title, i.Description, DateTime.UtcNow, i.AssignedTo == null ? null : i.AssignedTo.Name, i.Tags.Select(t =>t.Name).ToHashSet(), i.State,DateTime.UtcNow);
+
+        return items.FirstOrDefault();
     }
 
     public IReadOnlyCollection<WorkItemDTO> Read()
     {
-        throw new NotImplementedException();
+        var items = from i in _context.Items
+                     orderby i.Title
+                     select new WorkItemDTO(i.Id, i.Title, i.AssignedTo.Name, i.Tags.Select(t =>t.Name).ToHashSet(),i.State);
+
+        return items.ToArray();
     }
+
+    // public record WorkItemDTO(int Id, string Title, string AssignedToName, IReadOnlyCollection<string> Tags, State State);
+
 
     public IReadOnlyCollection<WorkItemDTO> ReadByState(State state)
     {
-        throw new NotImplementedException();
+        var workItems = from i in _context.Items
+                where i.State == state
+                select new WorkItemDTO(i.Id, i.Title, i.AssignedTo.Name, i.Tags.Select(t =>t.Name).ToHashSet(), i.State);
+
+        return workItems.ToList();
     }
 
     public IReadOnlyCollection<WorkItemDTO> ReadByTag(string tag)
     {
-        throw new NotImplementedException();
+        var workItems = from i in _context.Items
+                where i.Tags.Select(t =>t.Name).ToList<string>().AsReadOnly().Contains(tag)
+                select new WorkItemDTO(i.Id, i.Title, i.AssignedTo.Name, i.Tags.Select(t =>t.Name).ToHashSet(), i.State);
+
+        return workItems.ToList();
     }
 
     public IReadOnlyCollection<WorkItemDTO> ReadByUser(int userId)
     {
-        throw new NotImplementedException();
+          var workItems = from i in _context.Items
+                where i.AssignedTo.Id == userId
+                select new WorkItemDTO(i.Id, i.Title, i.AssignedTo.Name,i.Tags.Select(t =>t.Name).ToHashSet(), i.State);
+
+        return workItems.ToList();
     }
 
     public IReadOnlyCollection<WorkItemDTO> ReadRemoved()
     {
-        throw new NotImplementedException();
+        var workItems = from i in _context.Items
+                where i.State == State.Removed
+                select new WorkItemDTO(i.Id, i.Title, i.AssignedTo.Name, i.Tags.Select(t =>t.Name).ToHashSet(), i.State);
+
+        return workItems.ToList();
     }
 
     public Response Update(WorkItemUpdateDTO item)
     {
-        throw new NotImplementedException();
+
+    var entity = _context.Items.Find(item.Id);
+
+        if (entity == null)
+        {
+            return NotFound;
+        }
+
+        entity.Id = item.Id;
+        entity.Title = item.Title;
+        entity.AssignedToId = item.AssignedToId;
+        entity.Description = item.Description;
+        entity.Tags = CreateOrUpdateTags(item.Tags).ToHashSet();
+
+        //entity.Tags = item.Tags.Select(i =>i.Name).ToList<string>().AsReadOnly();
+        entity.State = item.State;
+
+        _context.SaveChanges();
+
+        return Updated;
+
+// public record WorkItemUpdateDTO(int Id, [StringLength(100)]string Title, int? AssignedToId, string? Description, ICollection<string> Tags, State State);
+
+
+    }
+
+    private User? CreateOrUpdateUser(string? userName, string? userEmail) {
+
+        return userName is null || userEmail is null ? null : _context.Users.FirstOrDefault(u => u.Name == userName && u.Email == userEmail) ?? new User(userName,userEmail);
+    } 
+
+    private IEnumerable<Tag> CreateOrUpdateTags(IEnumerable<string> tagNames)
+    {
+        var existing = _context.Tags.Where(t => tagNames.Contains(t.Name)).ToDictionary(t => t.Name);
+
+        foreach (var tagName in tagNames)
+        {
+            existing.TryGetValue(tagName, out var tag);
+
+            yield return tag ?? new Tag(tagName);
+        }
     }
 }
